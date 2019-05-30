b7af0b7750bfa6f4c378fa9bee43f9284c3cdc0f
liupeng <LIUPENG.0@outlook.com>
2019-05-30 17:25:32 +0800

    附.工具.数据库.提取数据 V 5.3.3

---
+++ src/Tools/LinkDatabases/GetDate.java
@@ -0,0 +1,56 @@
+package Tools.LinkDatabases;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GetDate {
+
+    private LinkDatabases linkDatabases = null;
+
+    private String sql = null;
+
+    private ResultSet resultSet = null;
+
+    public GetDate(String sql) throws SQLException, ClassNotFoundException {
+        this.init();
+        this.setSql(sql);
+        this.setResultSet(this.getLinkDatabases().getInformation(this.getSql()));
+    }
+
+    private void init() throws SQLException, ClassNotFoundException {
+        this.createLinkDatabases();
+        this.createSQL();
+    }
+
+    public void createLinkDatabases() throws SQLException, ClassNotFoundException {
+        this.linkDatabases = new LinkDatabases();
+    }
+
+    public LinkDatabases getLinkDatabases() {
+        return linkDatabases;
+    }
+
+    public void setLinkDatabases(LinkDatabases linkDatabases) {
+        this.linkDatabases = linkDatabases;
+    }
+
+    public void createSQL() {
+        this.sql = new String();
+    }
+
+    public String getSql() {
+        return sql;
+    }
+
+    public void setSql(String sql) {
+        this.sql = sql;
+    }
+
+    public ResultSet getResultSet() {
+        return resultSet;
+    }
+
+    public void setResultSet(ResultSet resultSet) {
+        this.resultSet = resultSet;
+    }
+}
