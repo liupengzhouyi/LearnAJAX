fec3c6c13fdf1e6bf9d4ebad480de03fc6e19d0b
liupeng <LIUPENG.0@outlook.com>
2019-05-30 20:06:23 +0800

    附.工具.数学.StringToDouble(重构) V 5.4.5

--- src/Tools/Math/StringToDouble.java
+++ src/Tools/Math/StringToDouble.java
@@ -6,49 +6,9 @@ public class StringToDouble {
 
     private double number;
 
-    private String integerNumber;
-
-    private String decimalNumber;
-
     public StringToDouble(String strNumber) {
         this.setStrNumber(strNumber);
-        System.out.println(strNumber);
-        String[] strings = this.getStrNumber().split("\\.");
-        System.out.println("strings.length:" + strings.length);
-        if (strings.length == 2) {
-            this.setIntegerNumber(strings[0]);
-            this.setDecimalNumber(strings[1]);
-        } else {
-            this.setIntegerNumber("0");
-            this.setDecimalNumber("0");
-        }
-        this.setNumber(this.getIntegerNumber() + this.getDecimalNumber());
-    }
-
-    public double getIntegerNumber() {
-        int tempNumber = Integer.getInteger(this.integerNumber);
-        double number = 0.0 + tempNumber;
-        return number;
-    }
-
-    public void setIntegerNumber(String integerNumber) {
-        this.integerNumber = integerNumber;
-    }
-
-    public double getDecimalNumber() {
-        int temp = Integer.getInteger(this.decimalNumber);
-        double tempNumber = 0.0 + temp;
-        int length = this.decimalNumber.length();
-        double myDivisor = 1;
-        for (int i=0;i<length;i++) {
-            myDivisor = myDivisor * 10;
-        }
-        double number = tempNumber / myDivisor;
-        return number;
-    }
-
-    public void setDecimalNumber(String decimalNumber) {
-        this.decimalNumber = decimalNumber;
+        this.setNumber(Double.valueOf(this.getStrNumber()));
     }
 
     public String getStrNumber() {
