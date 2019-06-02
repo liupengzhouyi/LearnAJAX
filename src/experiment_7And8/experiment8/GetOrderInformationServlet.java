8d21eb1fbcf37982f734cc91c070e78a2498a6fa
liupeng <LIUPENG.0@outlook.com>
2019-06-03 01:07:55 +0800

    实验八.后端.获取订单信息 V 10.4.5

--- src/experiment_7And8/experiment8/GetOrderInformationServlet.java
+++ src/experiment_7And8/experiment8/GetOrderInformationServlet.java
@@ -1,16 +1,26 @@
 package experiment_7And8.experiment8;
 
+import Tools.ReturnInformation.ReturnInformation;
+import experiment_1And2.experiment2.Good;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "GetOrderInformationServlet", urlPatterns = "/experiment_7And8/experiment8/GetOrderInformationServlet")
 public class GetOrderInformationServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        HttpSession httpSession = request.getSession();
+        String liupengGoodID = (String) httpSession.getAttribute("goodID");
+        String liupengGoodNumber = request.getParameter("liupengGoodNumbers");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println(liupengGoodID);
+        printWriter.println(liupengGoodNumber);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
